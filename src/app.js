de56c2a660c41db91fb6eac5f7922c396bb8c248
HeRui <877955100@qq.com>
2020-12-18 19:29:44 +0800

    init

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const koa = require('koa');
 const { server_port } = require("./config.json");
 const app = new koa();
-const asyncmw = require('./middleware/asyncmw');
 
-app.use(asyncmw());
 app.use(async (ctx) => {
     ctx.body = "hello koa2!"
 });
+
+module.exports = app;
 
 app.listen(server_port);
 console.log(`服务器已经启动在${server_port}端口`);
--- src/middleware/asyncmw.js
+++
@@ -1,7 +0,0 @@
-const log = (ctx) => { console.log(ctx.method, ctx.header.host + ctx.url) };
-module.exports = () => {
-    return async (ctx, next) => {
-        await next();
-        log(ctx);
-    }
-}
